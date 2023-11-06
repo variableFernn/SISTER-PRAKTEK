3b146523078b34a4325a9470aa7677a6eb69cb55
variableFernn <115528702+variableFernn@users.noreply.github.com>
2023-11-06 22:59:28 +0700

    Add files via upload

---
+++ st_2111083008/ST_2111083008.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package st_2111083008;
+
+/**
+ *
+ * @author Ferdi
+ */
+public class ST_2111083008 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
